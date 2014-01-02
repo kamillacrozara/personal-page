9b2f9036659b1e4d634992545ec0ec1a43c2b5f5
Kamilla <holanda.kamilla@gmail.com>
2014-01-02 16:28:17 -0500

    implementing REST methods

--- views.py
+++ views.py
@@ -9,11 +9,16 @@ class Index(MethodView):
 	def get(self):
 		return render_template('index.html')
 
-
 class Posts(MethodView):
 	def get(self):
 		entries = Post.objects.all()
 		return render_template('notes.html', entries=entries)
 
+	def post(self):
+		form = request.form
+		print form.title
+		return render_template('add_note.html')
+
 posts.add_url_rule('/', view_func=Index.as_view('index'))
 posts.add_url_rule('/notes', view_func=Posts.as_view('notes'))
+posts.add_url_rule('/add_note', view_func=Posts.as_view('add_note'), methods=['POST'])
