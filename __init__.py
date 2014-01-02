bd7d758faaf7a57023a864e58a81c70bf172af6c
Kamilla <holanda.kamilla@gmail.com>
2014-01-02 14:24:09 -0500

    REST API with Blueprints

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"DB": "personalpage"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
+
+
+def register_blueprints(app):
+    # Prevents circular imports
+    from personalpage.views import posts
+    app.register_blueprint(posts)
+
+register_blueprints(app)
+
+if __name__ == '__main__':
+    app.run()
---
+++ views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, redirect, render_template, url_for
+from flask.views import MethodView
+from personalpage.models import Post
+
+posts = Blueprint('posts', __name__, template_folder='templates')
+
+class Index(MethodView):
+
+	def get(self):
+		return render_template('index.html')
+
+
+class Posts(MethodView):
+	def get(self):
+		entries = Post.objects.all()
+		return render_template('notes.html', entries=entries)
+
+posts.add_url_rule('/', view_func=Index.as_view('index'))
+posts.add_url_rule('/notes', view_func=Posts.as_view('notes'))
